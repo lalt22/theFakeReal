2ddc577da9cb6df2864c90b528a0b94a082ee687
lalt22 <lalitha2seshadri@gmail.com>
2024-01-30 15:54:02 +1100

    stashing changes

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -2,11 +2,17 @@ import {useState, useContext, useEffect} from "react";
 import { getProductsInCart } from "../../../services/products";
 import CartList from "../../containers/CartList/CartList"
 import { RefreshContext } from "../../context/RefreshContextProvider";
+import CheckoutForm, { stripePromise } from "../../stripe/StripePayment";
+import { Elements } from "@stripe/react-stripe-js";
 
 const CartPage = () => {
     const [productsInCart, setProductsInCart] = useState(null);
 
     const {refresh} = useContext(RefreshContext);
+
+    const options = {
+        clientSecret: "{{CLIENT_SECRET}}",
+    }
 
     useEffect(() => {
         getProductsInCart().then((res) => {
@@ -15,9 +21,19 @@ const CartPage = () => {
     }, [refresh])
 
     return <main>
-        {productsInCart && <div>
-            <CartList products={productsInCart} />
-            </div>}
+        {productsInCart && 
+        <div>
+            <div>
+                <CartList products={productsInCart} />
+            </div>
+            <div className="payment_div">
+                <Elements stripe={stripePromise} options={options}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
+        </div>
+           
+        }
     </main>
 }
 
